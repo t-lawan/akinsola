de75069a9303bea67f838ed995873f397b2793bd
akinsola <thomas.lawanson@gmail.com>
2020-02-16 13:21:01 +0000

    installed packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Akinsola`,
+    description: `Akinsola Lawanson`,
+    author: `Akinsola`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -43,8 +43,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Akinsola`,
+        short_name: `akinsola`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
